5d8ed7d93f615b9de061c8827fd8907333e8cd1a
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-07-07 20:12:13 -0300

    teste

--- main.php
+++ main.php
@@ -14,13 +14,13 @@ function main(): void {
         $client = new Twitter;
         $payload = $dataHoje. "-" . $dias_da_semana[$diaSemana] . PHP_EOL . "Saladas: $cardapio[0]" . PHP_EOL . "Prato principal: " . $cardapio[1] . PHP_EOL . "Ovolactovegetariano: " . $cardapio[2] . PHP_EOL . "Guarnição: " . $cardapio[3] . PHP_EOL . "Acompanhamentos: " . $cardapio[4] . PHP_EOL . "Sobremesa: " . $cardapio[5];
         if (strlen($payload) > 280) {
-            $tweet1 = substr($payload, 0, 280);
-            $tweet2 = substr($payload, 280);
-            $response = $client->post_tweet($tweet1);
+            //$tweet1 = substr($payload, 0, 280);
+            //$tweet2 = substr($payload, 280);
+            $response = $client->post_tweet('docker1');
             print_r($response);
             $data = json_decode($response);
             $id = $data->tweet_id;
-            $response2 = $client->post_reply($tweet2, $id);
+            $response2 = $client->post_reply('docker2', $id);
             echo PHP_EOL;
             print_r($response2);
         } else {
