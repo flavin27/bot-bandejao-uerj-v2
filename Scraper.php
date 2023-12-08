9f566948891a723f311b8d92b7bbd6f7638578cc
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-12-08 15:51:29 -0300

    fix: added header

--- Scraper.php
+++ Scraper.php
@@ -43,6 +43,9 @@ class Scraper
         curl_setopt($ch, CURLOPT_URL, $this->url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        ]);
         $content = curl_exec($ch);
         curl_close($ch);
         return $content;
@@ -102,5 +105,6 @@ class Scraper
         return $cardapio[$dia];
     }
 }
+
 
 
