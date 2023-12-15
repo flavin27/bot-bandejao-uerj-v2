af9859e29345ea9433857d506584261cca4674f6
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-12-15 09:29:51 -0300

    teste

--- Scraper.php
+++ Scraper.php
@@ -41,11 +41,8 @@ class Scraper
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        ]);
         $content = curl_exec($ch);
         curl_close($ch);
         return $content;
